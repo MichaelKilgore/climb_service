729b28677b7977dbc3bb2389855a3526e05140a8
Michael Kilgore <michael@Michaels-MBP-3.lan>
2024-06-01 12:29:27 -0500

    Creating simple http server with hello endpoint that returns message Hello World

---
+++ src/hello_world.rs
@@ -0,0 +1,9 @@
+use::actix_web::{get, HttpResponse, http::header::ContentType};
+
+#[get("/hello")]
+pub async fn hello() -> HttpResponse {
+
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .json("{\"message\": \"hello world!\"}")
+}
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+mod hello_world;
+
+use actix_web::{App, HttpServer, middleware};
+use actix_rt::{main};
+use std::io;
+use hello_world::{hello};
+use std::env;
+
+#[main]
+async fn main() -> io::Result<()>  {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            // enable logger - always register actix-web Logger middleware last
+            .wrap(middleware::Logger::default())
+            // register HTTP requests handlers
+            .service(hello)
+    })
+        .bind("0.0.0.0:9090")?
+        .run()
+        .await
+}

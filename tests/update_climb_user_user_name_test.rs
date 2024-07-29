3fa2dee7952b6d3fbf70b26a6846f9a008256b82
Michael Kilgore <michael@Michaels-MBP-3.lan>
2024-07-28 21:58:53 -0500

    Making update climb user user name integration tests more readable

--- tests/update_climb_user_user_name_test.rs
+++ tests/update_climb_user_user_name_test.rs
@@ -1,389 +1,58 @@
-use std::env;
-use curl::easy::{Easy, List};
-use regex::Regex;
-use serde::Serialize;
-use serde_json::Value;
+mod utils;
 
-/*
- creates a user, changes there name to poopyjr, 
- then changes there name back to what it was previously 
- set to so this test can run and succeed again.
- */
+use serde_json::json;
+use utils::integ_tests_utils::IntegTestsUtilsImpl;
+use crate::utils::integ_tests_utils::IntegTestsUtils;
+
 #[test]
 fn test_update_climb_user_user_name() {
-    let mut easy = Easy::new();
+    let utils = IntegTestsUtilsImpl { };
 
-    // set host
-    let host = match env::var("SERVICE_URL") {
-        Ok(value) => {
-            value
-        }
-        Err(env::VarError::NotPresent) => {
-            "http://localhost:8080".to_string()
-        }
-        Err(env::VarError::NotUnicode(_)) => {
-            "http://localhost:8080".to_string()
-        }
-    };
-    easy.url(&format!("{host}/create-climb-user")).unwrap();
-    easy.post(true).unwrap();
+    // create user
+    let easy = utils.send_create_climb_user();
+    let response_body = utils.get_response_body(easy);
+    let climb_user_id = response_body.get("climb_user_id").unwrap().as_i64().unwrap();
+    let climb_user_name = response_body.get("user_name").unwrap().as_str().unwrap();
 
-    // set authentication header
-    let id_token = match env::var("ID_TOKEN") {
-        Ok(value) => {
-            value
-        }
-        Err(env::VarError::NotPresent) => {
-            "".to_string()
-        }
-        Err(env::VarError::NotUnicode(_)) => {
-            "".to_string()
-        }
-    };
-    let mut headers = List::new();
-    if !id_token.is_empty() {
-        headers.append(&format!("Authorization: Bearer {}", id_token)).unwrap();
-        easy.http_headers(headers).unwrap();
-    }
-
-    // perform request
-    easy.perform().unwrap();
-
-    let mut response_body = Vec::new();
-    {
-        let mut transfer = easy.transfer();
-        transfer.write_function(|data| {
-            response_body.extend_from_slice(data);
-            Ok(data.len())
-        }).unwrap();
-        transfer.perform().unwrap();
-    }
-
-    let actual_json: Value = serde_json::from_slice(&response_body).unwrap();
-
+    // update user name
+    let json_body = json!({
+        "climb_user_id": climb_user_id as i32,
+        "new_user_name": "poopyjr",
+    });
+    let mut easy = utils.send_update_climb_user_user_name(json_body);
     let response_code = easy.response_code().unwrap();
-
-    // Check if the request was successful
-    if response_code == 200 {
-        println!("Request was successful!");
-    } else {
-        println!("Request failed!");
-    }
-
     assert_eq!(response_code, 200);
 
-    if let Some(user_name) = actual_json.get("user_name").and_then(Value::as_str) {
-        if let Some(user_id) = actual_json.get("climb_user_id").and_then(Value::as_i64) {
-
-            let re = Regex::new(r"^user\d{20}$").unwrap();
-
-            assert_eq!(true, re.is_match(user_name));
-
-            let mut easy = Easy::new();
-
-            // set host
-            let host = match env::var("SERVICE_URL") {
-                Ok(value) => {
-                    value
-                }
-                Err(env::VarError::NotPresent) => {
-                    "http://localhost:8080".to_string()
-                }
-                Err(env::VarError::NotUnicode(_)) => {
-                    "http://localhost:8080".to_string()
-                }
-            };
-            easy.url(&format!("{host}/update-climb-user-user-name")).unwrap();
-            easy.post(true).unwrap();
-
-            // set authentication header
-            let id_token = match env::var("ID_TOKEN") {
-                Ok(value) => {
-                    value
-                }
-                Err(env::VarError::NotPresent) => {
-                    "".to_string()
-                }
-                Err(env::VarError::NotUnicode(_)) => {
-                    "".to_string()
-                }
-            };
-            let mut headers = List::new();
-            if !id_token.is_empty() {
-                headers.append(&format!("Authorization: Bearer {}", id_token)).unwrap();
-            }
-            headers.append("Content-Type: application/json").unwrap();
-            easy.http_headers(headers).unwrap();
-
-            // set json body request
-            #[derive(Serialize)]
-            struct UserNameUpdate {
-                climb_user_id: i32,
-                new_user_name: String,
-            }
-
-            let update = UserNameUpdate {
-                climb_user_id: user_id as i32,
-                new_user_name: "poopyjr".to_string(),
-            };
-
-            let json_data = serde_json::to_string(&update).unwrap();
-            easy.post_fields_copy(json_data.as_bytes()).unwrap();
-
-            // perform request
-            easy.perform().unwrap();
-
-            let response_code = easy.response_code().unwrap();
-
-            // Check if the request was successful
-            if response_code == 200 {
-                println!("Request was successful!");
-            } else {
-                println!("Request failed!");
-            }
-
-            assert_eq!(response_code, 200);
-
-
-            let update_back = UserNameUpdate {
-                climb_user_id: user_id as i32,
-                new_user_name: user_name.parse().unwrap(),
-            };
-            let json_data = serde_json::to_string(&update_back).unwrap();
-            easy.post_fields_copy(json_data.as_bytes()).unwrap();
-
-            easy.perform().unwrap();
-            let response_code = easy.response_code().unwrap();
-
-            // Check if the request was successful
-            if response_code == 200 {
-                println!("Request was successful!");
-            } else {
-                println!("Request failed!");
-            }
-
-            assert_eq!(response_code, 200);
-        } else {
-            panic!("no user_id extracted from response");
-        }
-    } else {
-        panic!("No user_name extracted from response");
-    }
+    // update username back to original username so this test can run again
+    let json_body = json!({
+        "climb_user_id": climb_user_id as i32,
+        "new_user_name": climb_user_name,
+    });
+    let mut easy = utils.send_update_climb_user_user_name(json_body);
+    let response_code = easy.response_code().unwrap();
+    assert_eq!(response_code, 200);
 }
 
 #[test]
 fn test_update_climb_user_user_name_fails_because_user_name_already_exists() {
-    let mut easy = Easy::new();
+    let utils = IntegTestsUtilsImpl { };
 
-    // set host
-    let host = match env::var("SERVICE_URL") {
-        Ok(value) => {
-            value
-        }
-        Err(env::VarError::NotPresent) => {
-            "http://localhost:8080".to_string()
-        }
-        Err(env::VarError::NotUnicode(_)) => {
-            "http://localhost:8080".to_string()
-        }
-    };
-    easy.url(&format!("{host}/create-climb-user")).unwrap();
-    easy.post(true).unwrap();
+    // create user_1    
+    let easy = utils.send_create_climb_user();
+    let response_body = utils.get_response_body(easy);
+    let climb_user_name_1 = response_body.get("user_name").unwrap().as_str().unwrap();
+    
+    // create user 2
+    let easy = utils.send_create_climb_user();
+    let response_body = utils.get_response_body(easy);
+    let climb_user_id_2 = response_body.get("climb_user_id").unwrap().as_i64().unwrap();
 
-    // set authentication header
-    let id_token = match env::var("ID_TOKEN") {
-        Ok(value) => {
-            value
-        }
-        Err(env::VarError::NotPresent) => {
-            "".to_string()
-        }
-        Err(env::VarError::NotUnicode(_)) => {
-            "".to_string()
-        }
-    };
-    let mut headers = List::new();
-    if !id_token.is_empty() {
-        headers.append(&format!("Authorization: Bearer {}", id_token)).unwrap();
-        easy.http_headers(headers).unwrap();
-    }
-
-    // perform request
-    easy.perform().unwrap();
-
-    let mut response_body = Vec::new();
-    {
-        let mut transfer = easy.transfer();
-        transfer.write_function(|data| {
-            response_body.extend_from_slice(data);
-            Ok(data.len())
-        }).unwrap();
-        transfer.perform().unwrap();
-    }
-
-    let actual_json: Value = serde_json::from_slice(&response_body).unwrap();
-
+    // attempt to change user 2's name to user 1's name
+    let json_body = json!({
+        "climb_user_id": climb_user_id_2 as i32,
+        "new_user_name": climb_user_name_1,
+    });
+    let mut easy = utils.send_update_climb_user_user_name(json_body);
     let response_code = easy.response_code().unwrap();
-
-    // Check if the request was successful
-    if response_code == 200 {
-        eprintln!("Request was successful!");
-    } else {
-        eprintln!("Request failed!");
-    }
-
-    assert_eq!(response_code, 200);
-
-    if let Some(user_name) = actual_json.get("user_name").and_then(Value::as_str) {
-        if let Some(user_id) = actual_json.get("climb_user_id").and_then(Value::as_i64) {
-
-            let re = Regex::new(r"^user\d{20}$").unwrap();
-
-            assert_eq!(true, re.is_match(user_name));
-
-            let mut easy = Easy::new();
-
-            // set host
-            let host = match env::var("SERVICE_URL") {
-                Ok(value) => {
-                    value
-                }
-                Err(env::VarError::NotPresent) => {
-                    "http://localhost:8080".to_string()
-                }
-                Err(env::VarError::NotUnicode(_)) => {
-                    "http://localhost:8080".to_string()
-                }
-            };
-            easy.url(&format!("{host}/create-climb-user")).unwrap();
-            easy.post(true).unwrap();
-
-            // set authentication header
-            let id_token = match env::var("ID_TOKEN") {
-                Ok(value) => {
-                    value
-                }
-                Err(env::VarError::NotPresent) => {
-                    "".to_string()
-                }
-                Err(env::VarError::NotUnicode(_)) => {
-                    "".to_string()
-                }
-            };
-            let mut headers = List::new();
-            if !id_token.is_empty() {
-                headers.append(&format!("Authorization: Bearer {}", id_token)).unwrap();
-                easy.http_headers(headers).unwrap();
-            }
-
-            // perform request
-            easy.perform().unwrap();
-
-            let mut response_body = Vec::new();
-            {
-                let mut transfer = easy.transfer();
-                transfer.write_function(|data| {
-                    response_body.extend_from_slice(data);
-                    Ok(data.len())
-                }).unwrap();
-                transfer.perform().unwrap();
-            }
-
-            let actual_json: Value = serde_json::from_slice(&response_body).unwrap();
-
-            let response_code = easy.response_code().unwrap();
-
-            // Check if the request was successful
-            if response_code == 200 {
-                eprintln!("Request was successful!");
-            } else {
-                eprintln!("Request failed!");
-            }
-
-            assert_eq!(response_code, 200);
-
-            if let Some(user_name_2) = actual_json.get("user_name").and_then(Value::as_str) {
-                if let Some(user_id_2) = actual_json.get("climb_user_id").and_then(Value::as_i64) {
-                    let re = Regex::new(r"^user\d{20}$").unwrap();
-
-                    assert_eq!(true, re.is_match(user_name_2));
-                    
-                    // attempt to set user_two's username to user_ones user_name
-                    let mut easy = Easy::new();
-
-                    // set host
-                    let host = match env::var("SERVICE_URL") {
-                        Ok(value) => {
-                            value
-                        }
-                        Err(env::VarError::NotPresent) => {
-                            "http://localhost:8080".to_string()
-                        }
-                        Err(env::VarError::NotUnicode(_)) => {
-                            "http://localhost:8080".to_string()
-                        }
-                    };
-                    easy.url(&format!("{host}/update-climb-user-user-name")).unwrap();
-                    easy.post(true).unwrap();
-
-                    // set authentication header
-                    let id_token = match env::var("ID_TOKEN") {
-                        Ok(value) => {
-                            value
-                        }
-                        Err(env::VarError::NotPresent) => {
-                            "".to_string()
-                        }
-                        Err(env::VarError::NotUnicode(_)) => {
-                            "".to_string()
-                        }
-                    };
-                    let mut headers = List::new();
-                    if !id_token.is_empty() {
-                        headers.append(&format!("Authorization: Bearer {}", id_token)).unwrap();
-                    }
-                    headers.append("Content-Type: application/json").unwrap();
-                    easy.http_headers(headers).unwrap();
-
-                    // set json body request
-                    #[derive(Serialize)]
-                    struct UserNameUpdate {
-                        climb_user_id: i32,
-                        new_user_name: String,
-                    }
-
-                    let update = UserNameUpdate {
-                        climb_user_id: user_id_2 as i32,
-                        new_user_name: user_name.parse().unwrap(),
-                    };
-
-                    let json_data = serde_json::to_string(&update).unwrap();
-                    easy.post_fields_copy(json_data.as_bytes()).unwrap();
-
-                    // perform request
-                    easy.perform().unwrap();
-
-                    let response_code = easy.response_code().unwrap();
-
-                    // Check if the request was successful
-                    if response_code == 200 {
-                        println!("Request was successful!");
-                    } else {
-                        println!("Request failed!");
-                    }
-
-                    assert_eq!(response_code, 409);
-
-                } else {
-                    panic!("Failed to get id from response");
-                }
-            } else {
-                panic!("Failed to get user_name from response");
-            }
-        } else {
-            panic!("Failed to get user_id from response");
-        }
-    } else {
-        panic!("Failed to get user_name from response");
-    }
+    assert_eq!(response_code, 409);
 }
--- tests/utils/integ_tests_utils.rs
+++ tests/utils/integ_tests_utils.rs
@@ -1,5 +1,6 @@
 use std::env;
 use curl::easy::{Easy, List};
+use serde::Serialize;
 use serde_json::Value;
 
 pub trait IntegTestsUtils {
@@ -12,9 +13,10 @@ pub trait IntegTestsUtils {
     fn send_create_climb_location_request(&self, json_body: Value) -> Easy;
 
     fn send_create_climb_user(&self) -> Easy;
-    
+
     fn send_hello(&self) -> Easy;
 
+    fn send_update_climb_user_user_name(&self, json_body: Value) -> Easy;
 }
 
 pub struct IntegTestsUtilsImpl;
@@ -62,7 +64,7 @@ impl IntegTestsUtils for IntegTestsUtilsImpl {
 
         return serde_json::from_slice(&response_body).unwrap();
     }
-    
+
     fn send_create_climb_location_request(&self, json_body: Value) -> Easy {
         let mut easy = Easy::new();
 
@@ -120,6 +122,29 @@ impl IntegTestsUtils for IntegTestsUtilsImpl {
         if !id_token.is_empty() {
             easy.http_headers(headers).unwrap();
         }
+
+        easy.perform().unwrap();
+
+        return easy;
+    }
+
+    fn send_update_climb_user_user_name(&self, json_body: Value) -> Easy {
+        let mut easy = Easy::new();        
+        
+        let host = self.get_host_url();
+
+        easy.url(&format!("{host}/update-climb-user-user-name")).unwrap();
+        easy.post(true).unwrap();
+        
+        let id_token = self.get_id_token();
+        let mut headers = List::new();
+        if !id_token.is_empty() {
+            headers.append(&format!("Authorization: Bearer {}", id_token)).unwrap();
+        }
+        headers.append("Content-Type: application/json").unwrap();
+        easy.http_headers(headers).unwrap();
+
+        easy.post_fields_copy(serde_json::to_string(&json_body).unwrap().as_bytes()).unwrap();
 
         easy.perform().unwrap();
 

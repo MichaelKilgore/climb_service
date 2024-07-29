cf22f01a86c73fe4e42ae26bba14a440ef152652
Michael Kilgore <michael@Michaels-MBP-3.lan>
2024-07-28 19:54:25 -0500

    Making create_climb_user_test integ test more readable

--- tests/create_climb_user_test.rs
+++ tests/create_climb_user_test.rs
@@ -1,84 +1,26 @@
-use std::env;
-use curl::easy::{Easy, List};
+mod utils;
+
 use regex::Regex;
-use serde_json::Value;
+use utils::integ_tests_utils::IntegTestsUtilsImpl;
+use crate::utils::integ_tests_utils::IntegTestsUtils;
 
 #[test]
 fn test_create_climb_user() {
-    let mut easy = Easy::new();
+    let utils = IntegTestsUtilsImpl { };
 
-    // set host
-    let host = match env::var("SERVICE_URL") {
-        Ok(value) => {
-            value
-        }
-        Err(env::VarError::NotPresent) => {
-            "http://localhost:8080".to_string()
-        }
-        Err(env::VarError::NotUnicode(_)) => {
-            "http://localhost:8080".to_string()
-        }
-    };
-    easy.url(&format!("{host}/create-climb-user")).unwrap();
-    easy.post(true).unwrap();
-
-    // set authentication header
-    let id_token = match env::var("ID_TOKEN") {
-        Ok(value) => {
-            value
-        }
-        Err(env::VarError::NotPresent) => {
-            "".to_string()
-        }
-        Err(env::VarError::NotUnicode(_)) => {
-            "".to_string()
-        }
-    };
-    let mut headers = List::new();
-    if !id_token.is_empty() {
-        headers.append(&format!("Authorization: Bearer {}", id_token)).unwrap();
-        easy.http_headers(headers).unwrap();
-    }
-
-    // perform request
-    easy.perform().unwrap();
-
-    let mut response_body = Vec::new();
-    {
-        let mut transfer = easy.transfer();
-        transfer.write_function(|data| {
-            response_body.extend_from_slice(data);
-            Ok(data.len())
-        }).unwrap();
-        transfer.perform().unwrap();
-    }
-
-    let actual_json: Value = serde_json::from_slice(&response_body).unwrap();
+    let mut easy = utils.send_create_climb_user();
 
     let response_code = easy.response_code().unwrap();
-    
-    // Check if the request was successful
-    if response_code == 200 {
-        println!("Request was successful!");
-    } else {
-        println!("Request failed!");
-    }
 
     assert_eq!(response_code, 200);
+    
+    let response_body = utils.get_response_body(easy);
+    
+    let user_name = response_body.get("user_name").unwrap().as_str().unwrap();
+    let re = Regex::new(r"^user\d{20}$").unwrap();
+    assert_eq!(true, re.is_match(&user_name));
 
-    if let Some(user_name) = actual_json.get("user_name").and_then(Value::as_str) {
-        let re = Regex::new(r"^user\d{20}$").unwrap();
-
-        assert_eq!(true, re.is_match(user_name));
-    } else {
-        panic!("Failed to get user_name from json response");
-    }
-
-    if let Some(user_id) = actual_json.get("climb_user_id").and_then(Value::as_i64) {
-        let re = Regex::new(r"^\d").unwrap();
-
-        assert_eq!(true, re.is_match(&*user_id.to_string()));
-    } else {
-        panic!("Failed to get id from json response");
-    }
+    let climb_user_id = response_body.get("climb_user_id").unwrap().as_i64().unwrap();
+    let re = Regex::new(r"^\d").unwrap();
+    assert_eq!(true, re.is_match(&*climb_user_id.to_string()));
 }
--- tests/utils/integ_tests_utils.rs
+++ tests/utils/integ_tests_utils.rs
@@ -1,12 +1,17 @@
 use std::env;
 use curl::easy::{Easy, List};
+use serde_json::Value;
 
 pub trait IntegTestsUtils {
     fn get_host_url(&self) -> String;
 
     fn get_id_token(&self) -> String;
 
-    fn send_create_climb_location_request(&self, json_body: serde_json::Value) -> Easy;
+    fn send_create_climb_location_request(&self, json_body: Value) -> Easy;
+    
+    fn send_create_climb_user(&self) -> Easy;
+    
+    fn get_response_body(&self, easy: Easy) -> Value;
 }
 
 pub struct IntegTestsUtilsImpl;
@@ -41,7 +46,7 @@ impl IntegTestsUtils for IntegTestsUtilsImpl {
         };
     }
 
-    fn send_create_climb_location_request(&self, json_body: serde_json::Value) -> Easy {
+    fn send_create_climb_location_request(&self, json_body: Value) -> Easy {
         let mut easy = Easy::new();
 
         let host = self.get_host_url();
@@ -63,4 +68,40 @@ impl IntegTestsUtils for IntegTestsUtilsImpl {
 
         return easy;
     }
+
+    fn send_create_climb_user(&self) -> Easy {
+        let mut easy = Easy::new();        
+        
+        let host = self.get_host_url();
+        
+        easy.url(&format!("{host}/create-climb-user")).unwrap();
+        easy.post(true).unwrap();
+
+        let id_token = self.get_id_token();
+
+        let mut headers = List::new();
+        if !id_token.is_empty() {
+            headers.append(&format!("Authorization: Bearer {}", id_token)).unwrap();
+            easy.http_headers(headers).unwrap();
+        }
+        
+        easy.perform().unwrap();
+
+        return easy;
+    }
+
+    fn get_response_body(&self, mut easy: Easy) -> Value {
+        let mut response_body = Vec::new();
+        {
+            let mut transfer = easy.transfer();
+            transfer.write_function(|data| {
+                response_body.extend_from_slice(data);
+                Ok(data.len())
+            }).unwrap();
+            transfer.perform().unwrap();
+        }
+
+        return serde_json::from_slice(&response_body).unwrap();
+    }
+    
 }
